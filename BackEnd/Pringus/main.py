5768fb07726f9a96b3c32dbea99423e3a370aafe
Joey <48295680+Colbalt01@users.noreply.github.com>
2022-11-30 18:13:06 -0700

    Edited the gets
    Was able to get info from MongoDB

--- BackEnd/Pringus/main.py
+++ BackEnd/Pringus/main.py
@@ -5,25 +5,78 @@ from flask import Flask, Response
 
 app = Flask(__name__)
 
+try:
+    mongo = pymongo.MongoClient(
+        "mongodb+srv://theGringus:SquingyDingus@pringus.kqhfvvx.mongodb.net/?retryWrites=true&w=majority")
+    db = mongo.Pringus
+    mongo.server_info()
+except:
+    print("ERROR - Cannot connect to db")
+
+
+@app.route('/planes', methods=["GET"])
+def getPlanes():
+    try:
+        planes = []
+        data = db.Planes
+        for plane in data.find():
+            planes.append(plane)
+        return str(planes)
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message": "cannot read name"}), status=500, mimetype="application/json")
+
 
 @app.route('/flights', methods=["GET"])
 def getFlights():
-    print("")
+    try:
+        flights = []
+        data = db.Flights
+        for flight in data.find():
+            flights.append(flight)
+        return str(flights)
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message": "cannot read name"}), status=500, mimetype="application/json")
 
 
 @app.route('/tickets', methods=["GET"])
 def getTickets():
-    print("")
+    try:
+        tickets = []
+        data = db.Tickets
+        for ticket in data.find():
+            tickets.append(ticket)
+        return str(tickets)
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message": "cannot read name"}), status=500, mimetype="application/json")
 
 
 @app.route('/users', methods=["GET"])
-def getFlights():
-    print("")
+def getUsers():
+    try:
+        users = []
+        data = db.Users
+        for user in data.find():
+            users.append(user)
+        return str(users)
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message": "cannot read name"}), status=500, mimetype="application/json")
 
 
 @app.route('/routes', methods=["GET"])
 def getRoutes():
-    print("")
+    try:
+        routes = []
+        data = db.Routes
+        for route in data.find():
+            routes.append(route)
+        return str(routes)
+    except Exception as ex:
+        print(ex)
+        return Response(response=json.dumps({"message": "cannot read name"}), status=500, mimetype="application/json")
 
 
 @app.route('/flights', methods=["POST"])
