268ce2ebe228d99232376a3f5c21300908f93e45
AMadmanWithABox <carter.gamble014@gmail.com>
2022-12-03 16:56:19 -0700

    Fixed the /planes/getAll endpoint

--- BackEnd/pringusSpring/src/main/java/com/example/pringusspring/WebSecurityConfig.java
+++ BackEnd/pringusSpring/src/main/java/com/example/pringusspring/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -37,6 +38,7 @@ public class WebSecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .cors().and()
                 .logout().deleteCookies().and()
                 .httpBasic();
