66654dc2328348d3205a02463bb8ce08c6d049fd
AMadmanWithABox <carter.gamble014@gmail.com>
2022-12-03 21:25:52 -0700

    fixed updateUser endpoint

--- BackEnd/pringusSpring/src/main/java/com/example/pringusspring/controller/UserRESTController.java
+++ BackEnd/pringusSpring/src/main/java/com/example/pringusspring/controller/UserRESTController.java
@@ -6,6 +6,7 @@ import com.example.pringusspring.repository.TicketRepository;
 import com.example.pringusspring.repository.UserRepository;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/users")
 public class UserRESTController {
-
+    private static final Logger LOGGER = org.slf4j.LoggerFactory.getLogger(UserRESTController.class);
     @Autowired
     private UserRepository userRepository;
 
@@ -43,16 +44,23 @@ public class UserRESTController {
     public void updateUser(@PathVariable String userId, @RequestBody ObjectNode objectNode) {
         User user;
         String[] ticketIds;
-        try {
-            user = objectNode.get("user").traverse().readValueAs(User.class);
-            ArrayNode arrayNode = (ArrayNode) objectNode.get("ticketIds");
-            ticketIds = new String[arrayNode.size()];
-            for (int i = 0; i < arrayNode.size(); i++) {
-                ticketIds[i] = arrayNode.get(i).asText();
-            }
-        } catch (IOException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid user");
+        LOGGER.info("Request body: " + objectNode.toString());
+        //public User (String email, String password, String firstName, String lastName, String role, String userID, String username, String[] tickets)
+        ArrayNode arrayNode = (ArrayNode) objectNode.get("tickets");
+        ticketIds = new String[arrayNode.size()];
+        for (int i = 0; i < arrayNode.size(); i++) {
+            ticketIds[i] = arrayNode.get(i).asText();
         }
+        user = new User(objectNode.get("user").get("email").asText(),
+                objectNode.get("user").get("password").asText(),
+                objectNode.get("user").get("firstName").asText(),
+                objectNode.get("user").get("lastName").asText(),
+                objectNode.get("user").get("role").asText(),
+                objectNode.get("user").get("userID").asText(),
+                objectNode.get("user").get("username").asText(),
+                ticketIds);
+
+        LOGGER.info("Request body: " + objectNode.asText());
         User userExists = userRepository.findByUserId(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
         if(userExists != null){
             List<Ticket> tickets = ticketRepository.findAllById(List.of(ticketIds));
